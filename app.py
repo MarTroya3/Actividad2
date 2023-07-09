e3bb054dbedb236b740e0c0dc8b97e7bf505f7df
MarTroya3 <132024686+MarTroya3@users.noreply.github.com>
2023-07-09 01:18:04 -0500

    Cambio

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 """
-    Ejemplo del manejo de hilos
+    Actividad 2 -Programacion Avanzada
 """
 
 import requests
 import time
 import csv
 import threading
+#Librería para ejecutar las operaciones en el sistema de archivos.
+import os
 # librería de python que permite ejecutar comandos
 import subprocess
 
@@ -14,12 +16,18 @@ def obtener_data():
     with open("informacion/data.csv") as archivo:
         lineas = csv.reader(archivo, quotechar="|")
         for row in lineas:
+            num = row[0].split("|")[0] #obtener el numero de link
+            url = row[0].split("|")[1] #obtener la url
+            # print("Iteracion: ", row," Numero: ", num," Direccion: ", url) 
+            #Comprobar el bucle
             # pass
-            # lista.append((numero, pagina))
+             lista.append([num, url]) #Agrega un nuevo pares elemento al final de la lista.
+
+    #print(lista) #comprobacion de que si esta guardando en list  
     # se retorna la lista con la información que se necesita
     return lista
 
-def worker(numero, url):
+def worker(num, url):
     print("Iniciando %s %s" % (threading.current_thread().getName(), url ))
     # pass
     time.sleep(10)
@@ -32,5 +40,5 @@ for c in obtener_data():
     url = c[1]
     hilo1 = threading.Thread(name='navegando...',
                             target=worker,
-                            args=(numero, url))
+                            args=(num, url))
     hilo1.start()
